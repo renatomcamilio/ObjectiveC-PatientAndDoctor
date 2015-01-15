f350693fcbafa9b061797943318e960b0d05b85c
Renato Camilio <renatomcamilio@gmail.com>
2015-01-15 10:20:28 -0800

    Initial Commit

---
+++ DoctorAndPatient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DoctorAndPatient
+//
+//  Created by Renato Camilio on 1/15/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
